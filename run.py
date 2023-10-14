6f16f42a888798e8b7e839a646bdd1f84bb5ca9b
amedix <amedix2@gmail.com>
2023-10-14 09:09:09 +0400

    map added

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import codecs
+import json
 
 
 app = Flask(__name__)
@@ -7,6 +8,13 @@ app = Flask(__name__)
 
 @app.route('/', methods=['get', 'post'])
 def index():
+    with codecs.open('data/offices.json', 'r', 'utf-8-sig') as f:
+        data = json.load(f)
+    print(data)
+
+    locations = [{'name': d['salePointName'], 'lat': d['latitude'], 'lon': d['longitude']} for d in data]
+    print(locations)
+
     if request.method == 'POST':
         checkbox1 = 'checkbox1' in request.form
         checkbox2 = 'checkbox2' in request.form
@@ -15,9 +23,10 @@ def index():
         checkbox1 = False
         checkbox2 = False
         checkbox3 = False
-
-    data = f'{checkbox1}, {checkbox2}, {checkbox3}'
-    return render_template('index.html', message=data, checked1=checkbox1, checked2=checkbox2, checked3=checkbox3)
+    ans = f'{checkbox1}, {checkbox2}, {checkbox3}'
+    return render_template('index.html', message=ans,
+                           checked1=checkbox1, checked2=checkbox2, checked3=checkbox3,
+                           locations=locations)
 
 
 if __name__ == '__main__':
