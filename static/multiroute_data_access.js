d882fc09b6d503e29014a56f9908f280ba1135ec
anal_Izhevsk <fazkek@yandex.ru>
2023-10-15 02:47:23 +0400

    autoTrip

--- static/multiroute_data_access.js
+++ static/multiroute_data_access.js
@@ -1,14 +1,13 @@
 function init () {
+    var yourLocation = [55.734876, 37.59308];
+
     // Создаем модель мультимаршрута.
-    var multiRouteModel = new ymaps.multiRouter.MultiRouteModel([
-            [55.734876, 37.59308],
-            "Москва, ул. Мясницкая"
-        ], {
+    var multiRouteModel = new ymaps.multiRouter.MultiRouteModel([], {
             // Путевые точки можно перетаскивать.
             // Маршрут при этом будет перестраиваться.
             wayPointDraggable: true,
             boundsAutoApply: true,
-            routingMode: "pedestrian"
+            routingMode: "masstransit"
         }),
 
         // Создаём выпадающий список для выбора типа маршрута.
@@ -18,8 +17,8 @@ function init () {
             },
             items: [
                 new ymaps.control.ListBoxItem({data: {content: "Авто"}}),
-                new ymaps.control.ListBoxItem({data: {content: "Общественным транспортом"}}),
-                new ymaps.control.ListBoxItem({data: {content: "Пешком"},state: {selected: true}})
+                new ymaps.control.ListBoxItem({data: {content: "Общественным транспортом"},state: {selected: true}}),
+                new ymaps.control.ListBoxItem({data: {content: "Пешком"}})
             ],
             options: {
                 itemSelectOnClick: false
@@ -53,6 +52,31 @@ function init () {
             yandexMapDisablePoiInteractivity: true
         }),
 
+        HintLayout = ymaps.templateLayoutFactory.createClass( "<div class='my-hint'>" +
+            "<b>{{ properties.object }}</b><br />" +
+            "{{ properties.address }}" +
+            "</div>", {
+                // Определяем метод getShape, который
+                // будет возвращать размеры макета хинта.
+                // Это необходимо для того, чтобы хинт автоматически
+                // сдвигал позицию при выходе за пределы карты.
+                getShape: function () {
+                    var el = this.getElement(),
+                        result = null;
+                    if (el) {
+                        var firstChild = el.firstChild;
+                        result = new ymaps.shape.Rectangle(
+                            new ymaps.geometry.pixel.Rectangle([
+                                [0, 0],
+                                [firstChild.offsetWidth, firstChild.offsetHeight]
+                            ])
+                        );
+                    }
+                    return result;
+                }
+            }
+        ),
+
         // Создаем на основе существующей модели мультимаршрут.
         multiRoute = new ymaps.multiRouter.MultiRoute(multiRouteModel, {
             // Путевые точки можно перетаскивать.
@@ -69,21 +93,37 @@ function init () {
 
     // Добавляем мультимаршрут на карту.
     myMap.geoObjects.add(multiRoute);
+    
 
     locations.forEach(function(location) {
-        var myPlacemark = new ymaps.Placemark([location.lat, location.lon], {balloonContent: location.adr+' ||| '+location.name}, {
+        var myPlacemark = new ymaps.Placemark([location.lat, location.lon], {
+            address: location.name,
+            object: location.adr        
+            }, {
             iconLayout: 'default#image',
-            // Своё изображение иконки метки.
             iconImageHref: 'http://127.0.0.1:5000/static/logo.png',
-            // Размеры метки.
             iconImageSize: [32, 32],
-            // Смещение левого верхнего угла иконки относительно
-            // её "ножки" (точки привязки).
             iconImageOffset: [-16, -16],
-            hideIconOnBalloonOpen: false
+            hideIconOnBalloonOpen: false,
+            hintLayout: HintLayout
         });
+    
         myMap.geoObjects.add(myPlacemark);
+    
+        myPlacemark.events.add('click', function (e) {
+            var lat = location.lat;
+            var lon = location.lon;
+            updateRouteEndPoint(lat, lon);
+        });
     });
+
+    function updateRouteEndPoint(lat, lon) {
+        multiRouteModel.setReferencePoints([yourLocation, [lat, lon]]);
+        console.log([lat, lon])
+        multiRouteModel.update().then(function () {
+            // Маршрут перестроен, можно выполнить дополнительные действия
+        });
+    }
 
     function changeRoutingMode(routingMode, targetItem) {
         multiRouteModel.setParams({ routingMode: routingMode }, true);
