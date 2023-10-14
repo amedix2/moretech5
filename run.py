afa36f4f1be3d32151602df4a1e74743f61d5453
amedix <amedix2@gmail.com>
2023-10-14 07:55:15 +0400

    fixed checkbox bug

--- run.py
+++ run.py
@@ -7,12 +7,18 @@ app = Flask(__name__)
 
 @app.route('/', methods=['get', 'post'])
 def index():
-    data = ''
     if request.method == 'POST':
-        data = request.form.get('data')
-    print(data)
-    return render_template('index.html', message=data)
+        checkbox1 = 'checkbox1' in request.form
+        checkbox2 = 'checkbox2' in request.form
+        checkbox3 = 'checkbox3' in request.form
+    else:
+        checkbox1 = False
+        checkbox2 = False
+        checkbox3 = False
+
+    data = f'{checkbox1}, {checkbox2}, {checkbox3}'
+    return render_template('index.html', message=data, checked1=checkbox1, checked2=checkbox2, checked3=checkbox3)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
